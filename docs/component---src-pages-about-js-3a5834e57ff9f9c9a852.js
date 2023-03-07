a67bcd5a966aa546e022f0cb892d8e6bf0c6c5b2
Oscar Westberg <oscarwestberg@users.noreply.github.com>
2023-03-07 15:52:57 +0100

    Put all public code in folder called docs so I can deploy and develop in the same branch.

---
+++ docs/component---src-pages-about-js-3a5834e57ff9f9c9a852.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoscarwestberg=self.webpackChunkoscarwestberg||[]).push([[682],{4633:function(e,n,t){t.r(n);var l=t(7294),r=t(1858);n.default=function(){return l.createElement(r.Z,null,l.createElement("div",{class:"content"},l.createElement("h1",null,"About"),l.createElement("p",null,"Swedish illustrator and comic creator."),l.createElement("p",null,"Currently open for business inquiries and personal commissions."),l.createElement("p",null,"westbergbusiness [at] gmail")))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-3a5834e57ff9f9c9a852.js.map
